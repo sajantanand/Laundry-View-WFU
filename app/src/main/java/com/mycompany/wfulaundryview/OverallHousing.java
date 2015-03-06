f42e00b5068617f79eba44db6c10b4618aa443ba
Sajant Anand <sajantanand@aol.com>
2015-03-06 16:09:28 -0500

    Mac's second commit

--- app/src/main/java/com/mycompany/wfulaundryview/OverallHousing.java
+++ app/src/main/java/com/mycompany/wfulaundryview/OverallHousing.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 
 import com.mycompany.wfulaundryview.Remote.LongmanAPIHelper;
 
-//Hello
+//Hello Try again
 
 public class OverallHousing extends ActionBarActivity {
 
