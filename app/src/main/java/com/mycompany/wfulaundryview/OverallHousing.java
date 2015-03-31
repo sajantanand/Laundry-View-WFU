ceb14a85caffc1c698cf5be589785df8e46df54b
doubl0kvn <kjdavis0201@gmail.com>
2015-03-31 13:04:42 -0400

    Housekeeping

--- app/src/main/java/com/mycompany/wfulaundryview/OverallHousing.java
+++ app/src/main/java/com/mycompany/wfulaundryview/OverallHousing.java
@@ -152,44 +152,6 @@ public class OverallHousing extends ActionBarActivity {
         startActivity(intent);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
